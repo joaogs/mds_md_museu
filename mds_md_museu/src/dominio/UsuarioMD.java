f129c1559f007c978ccbd2646777d46abdb2d619
simoes <joaosg22@gmail.com>
2019-06-21 14:29:01 -0300

    UsuarioMD

--- mds_md_museu/src/dominio/UsuarioMD.java
+++ mds_md_museu/src/dominio/UsuarioMD.java
@@ -1,7 +1,22 @@
 package dominio;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class UsuarioMD extends HttpServlet{
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String cmd = (String) request.getParameter("cmd");
+		
+		if (cmd == null) {
+			doGet(request,response);
+		} else {
+			
+		}
+	}
 
 }
